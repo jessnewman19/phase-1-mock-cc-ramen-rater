fbd0c5e730e01dfab8c31fe95faeab83e656ce66
Jessica <jess.newman19@gmail.com>
2022-04-14 09:53:07 -0400

    reworked, completed code

--- src/index.js
+++ src/index.js
@@ -1,50 +1,62 @@
 // write your code here
 
-//DECLARE GLOBAL VARIABLES
-const ramenDiv = document.querySelector("div") //Find the div to input the images
-// console.log(ramenDiv)
-const form = document.querySelector("form")
-// console.log(form)
-const ramenDetailImage = document.getElementsByClassName("detail-image")
-// console.log(ramenDetailImage)
 
+//SEE ALL RAMEN IMAGES IN THE DIV WITH ID OF RAMEN MENU
+// 1. Fetch the data 
+// 2. foreach loop through the data to grab the elements 
+// 3. Grab the ramen menu div
+// 4. Create an image element
+// 5. Add the img element to the ramen menu
 
+//CLICK ON IMAGE IN DIV AND SEE ALL INFO DISPLAYED INSIDE RAMEN DETAIL 
+// 1. Add click event listener to the image
+// 2. Grab the id for each ramen 
+// 3. Fetch new data
+// 4. Input 
+let ramenMenuDiv = document.getElementById('ramen-menu')  
 
-//DOMContentLoaded event listener added to the document. 
-document.addEventListener("DOMContentLoaded", (initialize)); 
+document.addEventListener('DOMContentLoaded', initialize)
 
-//Callback function used after DOM content is loaded
 function initialize () { 
-  fetch("http://localhost:3000/ramens")
+  fetch('http://localhost:3000/ramens')
   .then(response => response.json())
   .then(data => {
-    //Make sure I can GET the data
-    console.log(data)
-    //ADD THE PHOTOS FROM THE DATA TO THE PAGE
     data.forEach(element => {
-      let ramenImage = element.image 
-      // console.log(ramenImage) 
-      let img = document.createElement("img") //Create a new img for every element in the data
-      img.src = ramenImage //Set the source for the image to the data
-      img.id = element.id
-      ramenDiv.appendChild(img) //Append image to the div
+      let img = document.createElement("img")
+      img.src = element.image 
+      ramenMenuDiv.appendChild(img)
+
+      img.addEventListener("click", () => { 
+        let id = element.id
+        console.log(id)
+        fetch(`http://localhost:3000/ramens/${id}`)
+        .then(response => response.json())
+        .then(data => { 
+          document.querySelector(".detail-image").src = data.image
+          document.querySelector(".name").innerText = data.name
+          document.querySelector(".restaurant").innerText = data.restaurant
+          document.getElementById("rating-display").innerText = data.rating
+          document.getElementById("comment-display").innerText = data.comment
+        })
+      })  
     })
+  })
+}
+//CREATE NEW RAMEN AFTER SUBMITTING FORM
+// 1. Grab the form 
+// 2. Add an eventlistener on the form for a "submit"
+// 3. Grab the information from the form via e.target 
+// 4. Add the new information to the top 
 
-    //Add new ramen image to ramenDiv
-    form.addEventListener("submit", (e) => {
-      e.preventDefault() //Prevent page from refreshing
-      // console.log(e)
-      // console.log(e.target["new-image"].value)
-      let newRamenImg = document.createElement("img") //Create new ramenImg after submit
-      newRamenImg.src = e.target["new-image"].value //Find value of the newRamenImg
-      ramenDiv.appendChild(newRamenImg) //Append image to the div
-      form.reset()
-    })
-
-    //Add ramen information to detail area
-
-
-})
-
+const addNewRamen = () => { 
+  const form = document.getElementById("new-ramen")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let newImage = document.createElement("img")
+    newImage.src = e.target["new-image"].value
+    ramenMenuDiv.appendChild(newImage)
+    form.reset()
+  })
 }
 
+addNewRamen()
